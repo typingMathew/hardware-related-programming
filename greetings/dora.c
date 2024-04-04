42d1cd2cd4055709d6205ddd33381ac7ab805b4c
typingMathew <matteo.o.1106@gmail.com>
2024-04-04 11:15:48 +0200

    made project which uses wildcards

---
+++ greetings/dora.c
@@ -0,0 +1,47 @@
+// usage: anton numberOfGreetings delay
+#include<stdio.h>
+#include<stdlib.h>
+
+void work(int number, int delay);
+void active_wait(int delay);
+
+int main(int argc, char const *argv[])
+{
+    setbuf(stdout, 0); // disable stdout buffer
+    if (argc != 3)
+    {
+        printf("usage: %s numberOfGreetings delay", argv[0]);
+        return 1;
+    }
+
+    int number = atoi(argv[1]);
+    int delay = atoi(argv[2]);
+
+    work(number, delay);
+    printf("Done\n");
+    return 0;
+}
+
+void work(int number, int delay)
+{
+    for (size_t i = 0; i < number; i++)
+    {
+        printf("Hallo Dora!\t");
+        active_wait(delay);
+    }
+    
+}
+
+void active_wait(int delay)
+{
+    for (size_t i = 0; i < delay; i++)
+    {
+        for (size_t j = 0; j < delay; j++)
+        {
+            j++;
+            j--;
+        }
+        
+    }
+    
+}
