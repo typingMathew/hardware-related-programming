17e64b84241d33a7e8ead1a4015d9454425157c7
typingMathew <matteo.o.1106@gmail.com>
2024-10-13 16:17:43 +0200

    cleaned and fixed the programm

--- semaphore/schreiber.c
+++ semaphore/schreiber.c
@@ -5,16 +5,6 @@
 #include <sys/sem.h>
 #include <sys/wait.h>
 
-// Schlüsselwert der Semaphore,
-// damit sowohl Vater als auch Kind die gleiche referenzieren
-#define SEM_KEY 0x1111
-
-union semun {
-    int val;
-    struct semid_ds *buf;
-    unsigned short  *array;
-};
-
 // Hilfsstruktur zum Reservieren
 // Dekrementiert die Semaphore
 struct sembuf p = { 0, -1, SEM_UNDO};
@@ -24,36 +14,20 @@ struct sembuf v = { 0, +1, SEM_UNDO};
 
 void active_wait(size_t delay);
 
+// Erzeugt eine neue Semaphore und initialisiert ihren Wert auf init
+int create_new_semaphor(int init);
+
 
 int main(int argc, char const *argv[])
 {
     // Initialisierung
     srand(time(NULL));
     char c;
-    int ret_val;
-    // Erstellen der Semaphore
-    // Argument 2 bestimmt die Anzahl an Semaphoren (1)
-    // Argument 3 bestimmt den Modus: 0666 steht für die Berechtigungen 
-    //      (User-Group-All) --> (RW-RW-RW)
-    //      Außerdem wird durch IPC_CREAT festgelegt, dass eine neue Semaphore erstellt werden soll, falls noch keine vorhanden ist
-    ret_val = semget(SEM_KEY, 1, 0666 | IPC_CREAT);
-    // Fehlerbehandlung
-    if(ret_val < 0)
-    {
-        perror("semget fehlgeschlagen"); exit(11);
-    }
-    const int id = ret_val;
-    // Initialisieren der Semaphore mit dem Wert 1 (da eine Ressource)
-    union semun u;
-    u.val = 1;
-    // Setzen des Wertes der Semaphore auf den Wert von u.val
-    // dazu wird das Makro SETVAL genutzt
-    ret_val = semctl(id, 0, SETVAL, u);
-    // Fehlerbehandlung
-    if (ret_val < 0)
-    {
-        perror("semctl"); exit(12);
-    }
+    int ret_val, sem_write, sem_block;
+    // Identifikator der Semaphore zum Schreiben auf Standard-Out
+    const int sem_write_dad = create_new_semaphor(1);
+    // Identifikator der Semaphore zum Schreiben auf Standard-Out
+    const int sem_write_son = create_new_semaphor(0);
 
     // Gabelung des Programms
     int ret = fork();
@@ -63,25 +37,29 @@ int main(int argc, char const *argv[])
         perror("fork failed\n");
         return 1;
     }
-    // Fallunterscheidung das Programm ist Sohn
+    // Fallunterscheidung: das Programm ist Sohn
     else if (ret == 0)
     {
+        sem_write = sem_write_son;
+        sem_block = sem_write_dad;
         c = 's';
     }
-    // Fallunterscheidung das Programm ist Vater
+    // Fallunterscheidung: das Programm ist Vater
     else
     {
+        sem_write = sem_write_dad;
+        sem_block = sem_write_son;
         c = 'V';
     }
 
-    int count = 0;
-    for (size_t i = 0; i < 256; i++)
+    for (int i = 0; i < 256; i++)
     {
-        int w = rand() % 1000;
+        // Warten einer zufälligen Zeit
+        int w = rand() % 4000;
         active_wait(w);
-        // Versuchen die Semaphore zu bekommen
+        // Versuchen die eigene Semaphore zu bekommen
         // Dazu wird versucht die Operation p (Reservieren, Arg 2) einmal (Arg 3) auszuführen
-        ret_val = semop(id, &p, 1);
+        ret_val = semop(sem_write, &p, 1);
         // Fehlerbehandlung
         if(ret_val < 0)
         {
@@ -92,16 +70,14 @@ int main(int argc, char const *argv[])
         putchar(c);
         fflush(stdout);
         
-        // Freigeben der Semaphore
-        ret_val = semop(id, &v, 1);
+        // Freigeben der Semaphore für den anderen Prozess
+        ret_val = semop(sem_block, &v, 1);
         // Fehlerbehandlung
         if(ret_val < 0)
         {
             perror("semop p"); exit(13);
         }
         
-
-        count++;
     }
     wait(NULL);
     putchar('\n');
@@ -118,4 +94,32 @@ void active_wait(size_t delay)
             j--;
         }
     }
+}
+
+int create_new_semaphor(int init)
+{
+    int ret_val, sem_id;
+    // Erstellen der Semaphore
+    // Argument 2 bestimmt die Anzahl an Semaphoren (2)
+    // Argument 3 bestimmt den Modus: 0666 steht für die Berechtigungen 
+    //      (User-Group-All) --> (RW--)
+    //      Außerdem wird durch IPC_CREAT festgelegt, dass eine neue Semaphore erstellt werden soll, falls noch keine vorhanden ist
+    ret_val = semget(0, 1, 0600 | IPC_CREAT);
+    // Fehlerbehandlung
+    if(ret_val < 0)
+    {
+        perror("semget fehlgeschlagen"); exit(11);
+    }
+    sem_id = ret_val;
+
+    // Initialisieren der Semaphore mit dem Wert init
+    // dazu wird das Makro SETVAL genutzt
+    ret_val = semctl(sem_id, 0, SETVAL, init);
+    // Fehlerbehandlung
+    if (ret_val < 0)
+    {
+        perror("semctl"); exit(12);
+    }
+
+    return sem_id;
 }
